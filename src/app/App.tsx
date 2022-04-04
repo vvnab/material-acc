fc38d57c4d309a9858a739f4903158b16a4d15d9
vvnab <vvnab76@gmail.com>
2022-04-04 13:44:11 +0300

    --

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,7 +9,6 @@ function App() {
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                Deployed!
                 <Router />
             </PersistGate>
         </Provider>
