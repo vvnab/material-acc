974757b53c9009b98284ba5f4c0098cad660e866
vvnab <vvnab76@gmail.com>
2022-04-19 14:09:36 +0300

    --

--- src/features/admin/stock/warehouses/selectors.ts
+++ src/features/admin/stock/warehouses/selectors.ts
@@ -8,7 +8,12 @@ export const selectLoading = createSelector(
     (state) => state.loading
 );
 export const selectError = createSelector(selectDir, (state) => state.error);
-export const selectList = createSelector(selectDir, (state) => state.content);
+export const selectList = createSelector(selectDir, (state) =>
+    state.content.map((i) => ({
+        ...i,
+        materials: i.materials.filter((i) => i.quantity),
+    }))
+);
 export const selectItemLoading = createSelector(
     selectDir,
     (state) => state.itemLoading
