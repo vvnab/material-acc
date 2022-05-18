545d17bcf8ab143b3c93a60a69ba68631f59ed73
vvnab <vvnab76@gmail.com>
2022-05-18 11:55:02 +0300

    --

--- src/features/admin/reports/List.tsx
+++ src/features/admin/reports/List.tsx
@@ -1,37 +1,50 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { loadRequest } from "features/reports/actions";
-import { selectList, selectLoading } from "features/reports/selectors";
-import { loadRequest as loadBrigadesRequest } from "features/directories/brigades/actions";
-import FilterPanel from "./FilterPanel";
-import FlowItem from "./Item";
-import { Loader } from "common/components";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useInView } from 'react-intersection-observer';
+import { loadRequest, loadNextPageRequest } from 'features/reports/actions';
+import {
+    selectList,
+    selectLoading,
+    selectError,
+} from 'features/reports/selectors';
+import { loadRequest as loadBrigadesRequest } from 'features/directories/brigades/actions';
+import FilterPanel from './FilterPanel';
+import FlowItem from './Item';
+import { Loader } from 'common/components';
 
-import styles from "./List.module.scss";
+import styles from './List.module.scss';
 
 interface Props extends React.HTMLProps<HTMLDivElement> {}
 
 const Flows: React.FC<Props> = ({ children, ...rest }) => {
-  const dispatch = useDispatch();
-  const list = useSelector(selectList);
-  const loading = useSelector(selectLoading);
+    const dispatch = useDispatch();
+    const { ref, inView } = useInView();
+    const error = useSelector(selectError);
+    const list = useSelector(selectList);
+    const loading = useSelector(selectLoading);
 
-  useEffect(() => {
-    dispatch(loadRequest());
-    dispatch(loadBrigadesRequest());
-  }, [dispatch]);
-  
-  return (
-    <div {...rest} className={styles.wrap}>
-      <FilterPanel />
-      <div className={styles.list}>
-        {list.map((item) => (
-          <FlowItem key={item.id} {...item} />
-        ))}
-      </div>
-      {loading && <Loader className={styles.loader} />}
-    </div>
-  );
+    useEffect(() => {
+        inView && dispatch(loadNextPageRequest());
+    }, [inView, dispatch]);
+
+    useEffect(() => {
+        dispatch(loadRequest());
+        dispatch(loadBrigadesRequest());
+    }, [dispatch]);
+
+    return (
+        <div {...rest} className={styles.wrap}>
+            <FilterPanel />
+            <div className={styles.list}>
+                {list.map((item) => (
+                    <FlowItem key={item.id} {...item} />
+                ))}
+            </div>
+            <div ref={ref}></div>
+            {loading && <Loader className={styles.loader} />}
+            {error && <div className={styles.error}>{error}</div>}
+        </div>
+    );
 };
 
 export default Flows;
